cc452f821c1fa208c72dc2b922b53795804eb968
dreamatg <mha_atg@mha.gov.sg>
2021-08-15 23:23:54 +0800

    50% only

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import React, { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Do up react app',
+            day: '15 Aug All Day',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Wake up early and go to work by 7am',
+            day: '16 Aug at 6.30am',
+            reminder: true,
+        }
+
+    ])
+
+    const toggleReminder = (id) => {
+      setTasks(tasks.map((task) => task.id === id
+      ? { ...task, reminder: !task.reminder } : task 
+      ))}
+
+    // Delete Task
+    const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} 
+      onDelete={deleteTask}
+      onToggle={toggleReminder}        
+      >
+      </Tasks> : 'No tasks to show'}
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,18 @@
+import Task from './Task'
+
+const Tasks = (props) => {
+    const { tasks, onDelete, onToggle} = props
+
+    return (
+        <>
+        {tasks.map((task) => (
+        <Task key = {task.id} task={task}
+        onDelete={onDelete}
+        onToggle= {onToggle}
+        />
+        ))} 
+        </>
+    )
+}
+
+export default Tasks
